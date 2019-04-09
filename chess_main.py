fb1fa09fedcc98571bc0f810941b6df2ac1edf66
Anders Wallace <anderswallace@main-10-231-117-87.tamulink.tamu.edu>
2019-04-09 15:06:13 -0500

    added functionality to game

--- chess_main.py
+++ chess_main.py
@@ -5,17 +5,30 @@ import chess.engine
 
 # The move is set as a string from one space to another, i.e. "e5e6"
 # To quit the game, user can input 'q'
-def user_move(board, engine):
-	chess_move = input("User Move: ")
-	if chess_move == "q":
+def user_move(user_text, board):
+	
+	player_move = chess.Move.from_uci(user_text)
+	board.push(player_move)
+	print(" ")
+	print(board)
+	print(" ")
+	
+	return board
+
+def check_input(user_text, board):
+	if user_text == "q":
 		engine.quit()
 		print("\nQUITTING GAME")
 		quit()
+	elif user_text == "r":
+		print("\nRESTARTING GAME\n")
+		new_board = chess.Board()
+		return new_board
 	else:
-		player_move = chess.Move.from_uci(chess_move)
-		board.push(player_move)
-	
-	return
+		board = user_move(user_text, board)
+		return board
+
+
 
 def main():
 	engine = chess.engine.SimpleEngine.popen_uci("/Users/anderswallace/Documents/Documents/School/CSCE_462/462-Chess-Board/stockfish-10-64")
@@ -24,13 +37,16 @@ def main():
 	print("----- GAME START -----\n")
 
 	while not board.is_game_over():
-		result = engine.play(board, chess.engine.Limit(time=0.100))
+		result = engine.play(board, chess.engine.Limit(time=0.500))
 		board.push(result.move)
 		print("AI Move\n")
 		print(board)
 		print(" ")
-		user_move(board, engine)
+		user_choice = input("User Move: ")
+		board = check_input(user_choice, board)
+
 	engine.quit()
+
 
 main()
 
