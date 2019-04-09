ebf213a78b166508aab8bcab98fde4a88c6cf5c8
Anders Wallace <anderswallace@main-10-231-117-87.tamulink.tamu.edu>
2019-04-09 14:25:10 -0500

    engine is working

--- chess_main.py
+++ chess_main.py
@@ -1,7 +1,37 @@
-# File that is testing the chess board engine
-
 import chess
+import chess.engine
 
-board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+# This program acts as the AI that the user will play against
 
-print(board)
+# The move is set as a string from one space to another, i.e. "e5e6"
+# To quit the game, user can input 'q'
+def user_move(board, engine):
+	chess_move = input("User Move: ")
+	if chess_move == "q":
+		engine.quit()
+		print("\nQUITTING GAME")
+		quit()
+	else:
+		player_move = chess.Move.from_uci(chess_move)
+		board.push(player_move)
+	
+	return
+
+def main():
+	engine = chess.engine.SimpleEngine.popen_uci("/Users/anderswallace/Documents/Documents/School/CSCE_462/462-Chess-Board/stockfish-10-64")
+	board = chess.Board()
+
+	print("----- GAME START -----\n")
+
+	while not board.is_game_over():
+		result = engine.play(board, chess.engine.Limit(time=0.100))
+		board.push(result.move)
+		print("AI Move\n")
+		print(board)
+		print(" ")
+		user_move(board, engine)
+	engine.quit()
+
+main()
+
+
